98cbb0241c1b17d0acb8d092d685c34553092b97
vjmarcus <vjmarcus1@gmail.com>
2019-12-31 13:53:04 +0300

    Happy New Year 2020!

--- app/src/main/java/com/freshappbooks/aboutme/MainActivity.kt
+++ app/src/main/java/com/freshappbooks/aboutme/MainActivity.kt
@@ -2,11 +2,14 @@ package com.freshappbooks.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var image : ImageView
     }
 }
